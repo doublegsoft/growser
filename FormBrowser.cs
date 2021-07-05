eaaeb8ea37838dd81ad753e4799f735df28f12ae
Christian Gann <guo.guo.gan@gmail.com>
2021-07-05 18:00:45 +0800

    added javascript binding and toolbar

--- FormBrowser.cs
+++ FormBrowser.cs
@@ -29,7 +29,11 @@
 */
 using CefSharp;
 using CefSharp.WinForms;
+using Growser.HTTP;
+using Growser.JS;
 using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Windows.Forms;
 
 namespace Growser
@@ -39,25 +43,76 @@ namespace Growser
 
     private readonly ChromiumWebBrowser browser;
 
+    private static GrowserObject growserObject = new GrowserObject();
+
     public FormBrowser()
     {
+
+      NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+      foreach (NetworkInterface ni in interfaces)
+      {
+        Console.WriteLine(ni.NetworkInterfaceType);
+        Console.WriteLine(BitConverter.ToString(ni.GetPhysicalAddress().GetAddressBytes()));
+      }
+
       InitializeComponent();
 
+      CefSharpSettings.ConcurrentTaskExecution = true;
+      CefSharpSettings.FocusedNodeChangedEnabled = true;
+
       CefSettings settings = new CefSettings();
-      settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/GPMDP";
+      // settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/GPMDP";
       settings.WindowlessRenderingEnabled = true;
       settings.CefCommandLineArgs.Add("enable-smooth-scrolling", "1");
       settings.CefCommandLineArgs.Add("enable-overlay-scrollbar", "1");
       settings.CefCommandLineArgs.Add("high-dpi-support", "1");
       Cef.Initialize(settings);
 
-      browser = new ChromiumWebBrowser("https://ashapi.kangyangwang.net")
+      browser = new ChromiumWebBrowser("http://localhost")
       {
         RequestHandler = new DecryptedRequestHandler()
       };
       this.Container.ContentPanel.Controls.Add(browser);
 
-      // browser.RequestHandler = new DecryptedRequestHandler();
+      browser.JavascriptObjectRepository.ResolveObject += (sender, e) => 
+      {
+        var repo = e.ObjectRepository;
+        if (e.ObjectName == "growser")
+        {
+          repo.Register("growser", growserObject, true);
+        }
+      };
+
+      browser.JavascriptObjectRepository.ObjectBoundInJavascript += (sender, e) =>
+      {
+        // MessageBox.Show("绑定对象");
+      };
+    }
+
+    private void toolDebug_Click(object sender, EventArgs e)
+    {
+      browser.ShowDevTools();
+    }
+
+    private void toolExit_Click(object sender, EventArgs e)
+    {
+      DialogResult result = MessageBox.Show("确定要退出工作站？", "询问", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+      if (result.Equals(DialogResult.OK))
+      {
+        Application.Exit();
+      }     
+    }
+
+    private void FormBrowser_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      if (e.CloseReason == CloseReason.UserClosing)
+      {
+        DialogResult result = MessageBox.Show("确定要退出工作站？", "询问", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+        if (!result.Equals(DialogResult.OK))
+        {
+          e.Cancel = true;
+        }
+      }
     }
   }
 
