7a0f66c4da8bed59d8df09dede3b8855e37031bd
Christian Gann <guo.guo.gan@gmail.com>
2021-07-05 23:11:42 +0800

    renamed growser to growser-browser

---
+++ growser-browser/JS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Growser.Browser.JS
+{
+  public class GrowserObject
+  {
+
+    private Dictionary<string, object> userdata = new Dictionary<string, object>();
+
+    public void Show(string message)
+    {
+      MessageBox.Show(message);
+    }
+
+    public void SetUser(ExpandoObject user)
+    {
+      IDictionary<string, object> dict = (IDictionary<string, object>)user;
+      foreach (string key in dict.Keys) 
+      {
+        userdata[key] = dict[key];
+      }
+    }
+
+    public void GetUser()
+    {
+      MessageBox.Show((string)userdata["fullname"]);
+    }
+  }
+}
