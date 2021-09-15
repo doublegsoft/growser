f48d7bc53ab5886c3c1b3681fa6ef1d0eaf3ccd5
Christian Gann <guo.guo.gan@gmail.com>
2021-09-15 14:11:24 +0800

    changed

--- growser-his/Spec.cs
+++ growser-his/Spec.cs
@@ -18,19 +18,17 @@ namespace Growser.HIS
   public interface HISSpecification
   {
 
+    #region 配置数据
+
     /// <summary>
     /// 获取医院标识，医院标识是指医院在妇幼专科联盟中的编码。
     /// </summary>
     /// <returns>医院标识</returns>
     string GetHospitalId();
 
-    /// <summary>
-    /// 获取排队等候用户列表。
-    /// </summary>
-    /// <param name="hospitalId">医院标识</param>
-    /// <param name="clinicId">门诊科室标识</param>
-    /// <returns>等候用户</returns>
-    List<Patient> GetWaitingPatients(string clinicId);
+    #endregion
+
+    #region 医院基础数据
 
     /// <summary>
     /// 获取医院检查报告列表。
@@ -49,6 +47,29 @@ namespace Growser.HIS
     /// </summary>
     /// <returns>药品列表</returns>
     List<Medicine> GetMedicines();
+
+    /// <summary>
+    /// 查询HIS患者基本信息。
+    /// </summary>
+    /// <param name="patientName">患者姓名</param>
+    /// <param name="idCardNumber">身份证号</param>
+    /// <param name="mobile">手机号</param>
+    /// <param name="genderCode">性别编码</param>
+    /// <param name="birthDate">出生日期</param>
+    /// <returns></returns>
+    Patient GetPatient(string patientName, string idCardNumber = "", string mobile = "", string genderCode = "", string birthDate = "");
+
+    #endregion
+
+    #region 医生交互用例
+
+    /// <summary>
+    /// 获取排队等候用户列表。
+    /// </summary>
+    /// <param name="hospitalId">医院标识</param>
+    /// <param name="clinicId">门诊科室标识</param>
+    /// <returns>等候用户</returns>
+    List<Patient> GetWaitingPatients(string clinicId);
 
     /// <summary>
     /// 计算检查/检验单价格。
@@ -72,8 +93,8 @@ namespace Growser.HIS
     /// <param name="outpatientNumber">门诊号</param>
     /// <param name="clinicId">开单科室标识</param>
     /// <param name="attendingDoctorId">主诊医生标识</param>
-    /// <param name="tests">检查单</param>
-    void CreateTestReports(string patientId, string outpatientNumber, string clinicId, string attendingDoctorId, List<MedicalTestReport> tests);
+    /// <param name="medicalTestReports">检查单</param>
+    List<PatientTestReport> CreateTestReports(string patientId, string outpatientNumber, string clinicId, string attendingDoctorId, List<MedicalTestReport> medicalTestReports);
 
     /// <summary>
     /// 开具检验单。
@@ -82,8 +103,11 @@ namespace Growser.HIS
     /// <param name="outpatientNumber">门诊号</param>
     /// <param name="clinicId">开单科室标识</param>
     /// <param name="attendingDoctorId">主诊医生标识</param>
-    /// <param name="laboratoryTests">检验单</param>
-    void CreateLaboratoryReports(string patientId, string outpatientNumber, string clinicId, string attendingDoctorId, List<MedicalLaboratoryReport> laboratoryTests);
+    /// <param name="medicalLaboratoryReports">检验单</param>
+    /// <returns>持久化后的患者报告</returns>
+    /// <remarks>
+    /// </remarks>
+    List<PatientLaboratoryReport> CreateLaboratoryReport(string patientId, string outpatientNumber, string clinicId, string attendingDoctorId, List<MedicalLaboratoryReport> medicalLaboratoryReports);
 
     /// <summary>
     /// 开具处方。
@@ -93,8 +117,37 @@ namespace Growser.HIS
     /// <param name="clinicId">开单科室标识</param>
     /// <param name="attendingDoctorId">主诊医生标识</param>
     /// <param name="prescription">处方对象</param>
-    /// <returns>处方标识</returns>
-    void CreatePrescription(string patientId, string outpatientNumber, string clinicId, string attendingDoctorId, Prescription prescription);
+    /// <returns>持久化后的处方</returns>
+    Prescription CreatePrescription(string patientId, string outpatientNumber, string clinicId, string attendingDoctorId, Prescription prescription);
+
+    /// <summary>
+    /// 获取患者门诊的检查报告。
+    /// </summary>
+    /// <param name="patientId">患者标识</param>
+    /// <param name="outpatientNumber">门诊号</param>
+    /// <param name="reportNumber">报告单号</param>
+    /// <returns>门诊检查报告</returns>
+    List<PatientTestReport> GetPatientTestReports(string patientId, string outpatientNumber = "", string reportNumber = "");
+
+    /// <summary>
+    /// 获取患者门诊的检验报告。
+    /// </summary>
+    /// <param name="patientId">患者标识</param>
+    /// <param name="outpatientNumber">门诊号</param>
+    /// <param name="reportNumber">报告单号</param>
+    /// <returns>门诊检验报告</returns>
+    List<PatientLaboratoryReport> GetPatientLaboratoryReports(string patientId, string outpatientNumber = "", string reportNumber = "");
+
+    Outpatient MakeDiagnosis(string patientId, string diagnosisCode, string diagnosis);
+
+    /// <summary>
+    /// 获取患者门诊信息。
+    /// </summary>
+    /// <param name="patientId">患者标识</param>
+    /// <returns>门诊信息</returns>
+    List<Outpatient> GetOutpatients(string patientId);
+
+    #endregion
   }
 
   public class HISSpecificationHost
